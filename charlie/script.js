a7202b7e37731ce9a99bf930cba79e3cbc021bf5
DaVas255 <101551998+DaVas255@users.noreply.github.com>
2023-03-14 20:40:00 +0700

    js/overlay fix

--- charlie/script.js
+++ charlie/script.js
@@ -1,23 +1,17 @@
-function modal() {
-  const modal = document.querySelector("#pop-up");
-  const overlay = document.querySelector("#overlay");
-  const closeButton = document.querySelector("#js-close-button");
-  const modalTriggers = document.querySelectorAll("#js-button-open");
+const modal = document.querySelector("#pop-up");
+const overlay = document.querySelector("#overlay");
+const closeButton = document.querySelector("#js-close-button");
+const openButton = document.querySelector("#js-button-open");
 
-  const openModal = function () {
-    modal.classList.add("is-open");
-    overlay.classList.add("is-open");
-  };
+const openModal = function () {
+  modal.classList.add("is-open");
+  overlay.classList.add("is-open");
+};
 
-  const closeModal = function () {
-    modal.classList.remove("is-open");
-    overlay.classList.remove("is-open");
-  };
+const closeModal = function () {
+  modal.classList.remove("is-open");
+  overlay.classList.remove("is-open");
+};
 
-  modalTriggers.forEach(function (item) {
-    item.addEventListener("click", openModal);
-  });
-
-  closeButton.addEventListener("click", closeModal);
-  overlay.addEventListener("click", closeModal);
-}
+openButton.addEventListener("click", openModal);
+closeButton.addEventListener("click", closeModal);
